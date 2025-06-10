b193c9ac645c83b96a3104d5ab0ba4f3c224a8d9
Narryss <144238002+Narryss@users.noreply.github.com>
2025-06-10 19:02:21 +0300

    Update tvoe_spider.py

--- tvoe/spiders/tvoe_spider.py
+++ tvoe/spiders/tvoe_spider.py
@@ -1,6 +1,5 @@
 import scrapy
 from scrapy.spiders import CrawlSpider
-from openpyxl import Workbook
 from tvoe.items import TvoeItem
 
 class TvoeSpiderSpider(CrawlSpider):
@@ -18,26 +17,32 @@ class TvoeSpiderSpider(CrawlSpider):
             yield response.follow(women_link, callback=self.parse_category)
 
     def parse_category(self, response):
-        product_links = response.xpath('//div[@class="product-list"]'
-                                       '//div[contains(@class, "product-list__product-wrapper")]'
-                                       '//a[@class="product__image-link"]/@href').getall()
+        product_links = response.xpath('//component[contains(@class, "product__title")]/@link').getall()
         for link in product_links:
             yield response.follow(link, callback=self.parse_item)
 
-        pagination_links = response.xpath('//div[@class="pagination"]//a[@class="pagination__list-item"]/@href').getall()
-        for link in pagination_links:
-            yield response.follow(link, callback=self.parse_category)
+        page_numbers = response.xpath('//a[@class="pagination__list-item"]/text()').getall()
+
+        if page_numbers:
+            try:
+                last_page = int(page_numbers[-1])
+            except ValueError:
+                last_page = 1
+        else:
+            last_page = 1
+
+        base_url = response.url.split('?')[0]
+        for page in range(2, last_page + 1):
+            page_url = f"{base_url}?page={page}"
+            yield response.follow(page_url, callback=self.parse_category)
 
     def parse_item(self, response):
         item = TvoeItem()
 
-        price_item = response.xpath('.//div[@class="product-detail__prices"]')
-
         item['url'] = response.url
 
         breadcrumbs = []
-        for breadcrumb in response.xpath('//div[@class="breadcrumbs breadcrumbs--product-detail"]'
-                                         '//a[@class="breadcrumbs__item-link"]'):
+        for breadcrumb in response.xpath('//div[contains(@class, "breadcrumbs__item")]'):
             breadcrumb_text = breadcrumb.xpath('.//span[@class="breadcrumbs__item-text"]/text()').get()
             if breadcrumb_text:
                 breadcrumbs.append(breadcrumb_text.strip())
@@ -49,35 +54,31 @@ class TvoeSpiderSpider(CrawlSpider):
         item['description'] = response.xpath('.//div[@class="product-detail"]'
                                              '//div[@class="product-detail__description text-b11"]/text()').get()
 
-        discount_price = price_item.xpath('.//span[@class="product-detail__price product-detail__price--discount"]'
+        discount_price = response.xpath('.//span[@class="product-detail__price product-detail__price--discount"]'
                                           '/text()').get()
         if discount_price:
-            item["current_price"] = discount_price.strip()
+            item["current_price"] = discount_price.replace('\xa0', '').strip()
         else:
-            regular_price = price_item.xpath('.//span[@class="product-detail__price"]/text()').get()
-            item["current_price"] = regular_price.strip() if regular_price else None
+            regular_price = response.xpath('.//span[@class="product-detail__price"]/text()').get()
+            item["current_price"] = regular_price.replace('\xa0', '').strip() if regular_price else None
 
-        old_price = price_item.xpath('.//span[@class="product-detail__price product-detail__price--old"]'
+        old_price = response.xpath('.//span[@class="product-detail__price product-detail__price--old"]'
                                      '/text()').get()
-        item["old_price"] = old_price.strip() if old_price else None
+        item["old_price"] = old_price.replace('\xa0', '').strip() if old_price else None
 
         attributes = []
-        for attribute in response.xpath('.//div[@class="product-detail__attribute"]'):
-            attribute_name = attribute.xpath('.//span[@class="product-detail__attribute-t"]/text()').get()
-            attribute_value = attribute.xpath('.//span[@class="product-detail__attribute-d"]/text()').get()
+        attributes_block = response.xpath('//div[@class="product-detail__attribute"]')
+        for attribute in attributes_block:
+            attribute_name = attribute.xpath('./span[contains(@class, "product-detail__attribute-t")]/text()').get()
+            attribute_value = attribute.xpath('./span[contains(@class, "product-detail__attribute-d")]/text()').get()
             if attribute_name and attribute_value:
                 attributes.append({
-                    'name': attribute_name.strip(),
+                    'name': attribute_name.strip().rstrip(':'),
                     'value': attribute_value.strip()
                 })
         item['attributes'] = attributes
 
-        images = []
-        for img in response.xpath('.//div[@class="product-detail__gallery-inner"]//img'):
-            img_url = img.xpath('@src').get()
-            if img_url:
-                images.append(img_url.strip())
-
+        images = response.xpath('.//img[contains(@class,"product-detail__image")]/@src').getall()
         item['images'] = images
 
         yield item
